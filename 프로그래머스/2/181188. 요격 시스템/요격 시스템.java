569f0fada4ae8016ce0b40f278406ed10f133735
SeungyeonLee <leesyseel@gmail.com>
2026-03-01 15:40:28 +0900

    [level 2] Title: 요격 시스템, Time: 473.53 ms, Memory: 198 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,3 +1,4 @@
+/*
 import java.util.*;
 
 class Solution {
@@ -20,6 +21,25 @@ class Solution {
                 }
             }
         }
+        return answer;
+    }
+}
+*/
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 1;
+        
+        Arrays.sort(targets, (arr1, arr2) -> Integer.compare(arr1[1], arr2[1]));
+        int end = targets[0][1];
+        for(int[] target : targets) {
+            if(target[0] < end) continue;
+            
+            answer++;
+            end = target[1];
+        }
+        
         return answer;
     }
 }
